ea27f37f842c1b4028b0eabac6b83672477a57cd
ToukaMer <46601512+ToukaMer@users.noreply.github.com>
2021-05-13 11:29:11 +0200

    Integration of php to the index buttons
    The actions of recherche rapide and recherche avancée are defined :
    in case of product not found => produit_404
    several products found => liste_produits
    one product is found => fiche_produit

---
+++ traitement.php
@@ -0,0 +1,27 @@
+<?php 
+require 'vendor/autoload.php';
+$client = new MongoDB\Client(
+    'mongodb+srv://Yosra:iaIqRPWxXN9AsFuF@cluster0.bbe6n.mongodb.net/PRODUITS_DB?retryWrites=true&w=majority');
+$db = $client->PRODUITS_DB;
+$collection = $client->$db->PRODUITS;
+$cursor=$collection->find( array( "product_name" => $_POST['search_name']));
+$i=0;
+foreach($cursor as $document) {
+//    print_r($document);
+	$i=$i+1;
+}
+if (isset($_POST['avancee'])) {
+	header('Location: recherche_avancee.html');
+}
+else {
+	if ($i>1) {
+		header('Location: liste_produits.html');
+	}
+	elseif ($i==1) {
+		header('Location: fiche_produit.html');
+	}
+	elseif ($i==0) {
+		header('Location: produit_404.html');
+	}
+}
+?>
