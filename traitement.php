575ee3a2e65a2f6c8782f3b1a6ce2ffacda10b27
ToukaMer <46601512+ToukaMer@users.noreply.github.com>
2021-05-14 12:52:07 +0200

    Index and traitement.php fixed

--- traitement.php
+++ traitement.php
@@ -6,20 +6,22 @@ $client = new MongoDB\Client(
     'mongodb+srv://Yosra:iaIqRPWxXN9AsFuF@cluster0.bbe6n.mongodb.net/PRODUITS_DB?retryWrites=true&w=majority');
 $db = $client->PRODUITS_DB;
 $collection = $client->$db->PRODUITS;
-$cursor=$collection->find( array( "product_name" => $_POST['search_name']));
+$cursor=$collection->find( array( "product_name" => ""+$_POST['search_name']+"*"));
 $i=0;
 foreach($cursor as $document) {
 //    print_r($document);
 	$i=$i+1;
 }
-if ($i>1) {
-    header('Location: liste_produits.html');
-}
-elseif ($i==1) {
-	header('Location: fiche_produit.php');
-}
-elseif ($i==0) {
-	header('Location: produit_404.html');
+if (isset($_POST['rapide'])) {
+	if ($i>1) {
+		header('Location: liste_produits.html');
+	}
+	elseif ($i==1) {
+		header('Location: fiche_produit.php');
+	}
+	elseif ($i==0) {
+		header('Location: produit_404.html');
+	}
 }
 
 ?>
