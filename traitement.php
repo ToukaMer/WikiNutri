9828940df63c496760dc1ae0a87b33e8d3352b4e
ToukaMer <46601512+ToukaMer@users.noreply.github.com>
2021-05-14 13:18:15 +0200

    regex added to traitement.php

--- traitement.php
+++ traitement.php
@@ -6,10 +6,11 @@ $client = new MongoDB\Client(
     'mongodb+srv://Yosra:iaIqRPWxXN9AsFuF@cluster0.bbe6n.mongodb.net/PRODUITS_DB?retryWrites=true&w=majority');
 $db = $client->PRODUITS_DB;
 $collection = $client->$db->PRODUITS;
-$cursor=$collection->find( array( "product_name" => ""+$_POST['search_name']+"*"));
+$search_value = $_POST['search_name'];
+$regex = new MongoDB\BSON\Regex($search_value, 'i');
+$cursor=$collection->find( array( "product_name" => $regex));
 $i=0;
 foreach($cursor as $document) {
-//    print_r($document);
 	$i=$i+1;
 }
 if (isset($_POST['rapide'])) {
@@ -23,5 +24,4 @@ if (isset($_POST['rapide'])) {
 		header('Location: produit_404.html');
 	}
 }
-
 ?>
