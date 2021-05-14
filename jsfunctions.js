520d31c86ad56d670c16fb34c34d3181133413b5
NirmouMounir <15121247+NirmouMounir@users.noreply.github.com>
2021-05-14 14:41:09 +0200

    panel qui slide

--- jsfunctions.js
+++ jsfunctions.js
@@ -1,21 +1,5 @@
 $(document).ready(function(){
-    $("button[value='avancee']".click(function(){
-            $("#panel").slideUp();
-        })
-    );});
-
-function showHidePanel() {
-    if (window.getComputedStyle(document.getElementById("panel")).display === "block"){
-        document.getElementById("panel").style.display = "none";
-    } else {
-        document.getElementById("panel").style.display = "block";
-    }
-}
-
-function hidePanel(){
-    panel = document.getElementById("panel").style;
-    if(panel.hasClass("ferme")){
-        document.getElementById("panel");
-    }
-
-}
+    $(".slideToggle").click(function(){
+        $("#panel").slideToggle("slow");
+    });
+});
